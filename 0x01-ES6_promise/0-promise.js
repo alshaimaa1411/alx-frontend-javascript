0bb092686670d6b387dbbca362b7693d95d4edc9
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-10-23 12:47:41 +0000

    t

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,14 @@
+function getResponseFromAPI(){
+  let myPromise = new Promise(function(myResolve, myReject) {
+
+  myResolve(); 
+  myReject();  
+});
+
+myPromise.then(
+  function(value) {true},
+  function(error) {false}
+);
+return myPromise;
+}
+
