b42d86bbeeb096f1ded2104f4cb8f5a64cfde025
Momen Ahmed <momen.ahmed771647@gmail.com>
2024-10-27 16:44:13 +0200

    s

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,7 +2,8 @@ import {uploadPhoto, createUser} from "./utils.js"
 export default async function handleProfileSignup() {
   const pro1 = await uploadPhoto();
   const pro2 = await createUser();
-  return new Promise.all([pro1, pro2])
+  const mypro = new Promise.all([pro1, pro2]);
+  return mypro
     .then(() => (console.log(pro1.body, pro2.firstName, pro2.lastName)))
     .catch(() => (new Error('Signup system offline')));
 }
